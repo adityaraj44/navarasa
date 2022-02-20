939992c91230d3885e4d3654df15d7ba92d89f90
adityaraj44 <brockjuly852@gmail.com>
2022-02-20 17:14:39 +0530

    validation fixed

--- client/src/components/SubmitEntry/SubmitInfo.js
+++ client/src/components/SubmitEntry/SubmitInfo.js
@@ -24,7 +24,19 @@ const SubmitInfo = () => {
   const history = useHistory();
 
   const formContext = useContext(FormFieldContext);
-  const { formFields, setFormFields } = formContext;
+  const {
+    formFields,
+    setFormFields,
+    validSubmittername,
+    validRole,
+    validEmail,
+    validCountry,
+    validContact,
+    validState,
+    validCity,
+    validAddress,
+    validateFields,
+  } = formContext;
 
   const handleOnChange = (e) => {
     setFormFields({
@@ -33,17 +45,10 @@ const SubmitInfo = () => {
     });
   };
 
-  const [validSubmittername, setValidSubmittername] = React.useState(null);
-  const [validRole, setValidRole] = React.useState(null);
-  const [validEmail, setValidEmail] = React.useState(null);
-  const [validCountry, setValidCountry] = React.useState(null);
-  const [validContact, setValidContact] = React.useState(null);
-  const [validState, setValidState] = React.useState(null);
-  const [validCity, setValidCity] = React.useState(null);
-  const [validAddress, setValidAddress] = React.useState(null);
-
   const handleInfoContinue = (e) => {
     e.preventDefault();
+    validateFields();
+
     if (
       validSubmittername === true &&
       validRole === true &&
@@ -55,95 +60,6 @@ const SubmitInfo = () => {
       validAddress === true
     ) {
       history.push("/submitsong");
-    }
-    if (
-      formFields.submittername === "" ||
-      formFields.submittername === null ||
-      formFields.submittername === undefined ||
-      formFields.submittername === " " ||
-      formFields.submittername.length < 1
-    ) {
-      setValidSubmittername(false);
-    } else {
-      setValidSubmittername(true);
-    }
-    if (
-      formFields.role === "" ||
-      formFields.role === null ||
-      formFields.role === undefined ||
-      formFields.role === " " ||
-      formFields.role.length < 1
-    ) {
-      setValidRole(false);
-    } else {
-      setValidRole(true);
-    }
-    if (
-      formFields.email === "" ||
-      formFields.email === null ||
-      formFields.email === undefined ||
-      formFields.email === " " ||
-      formFields.email.length < 1
-    ) {
-      setValidEmail(false);
-    } else {
-      setValidEmail(true);
-    }
-    if (
-      formFields.country === "" ||
-      formFields.country === null ||
-      formFields.country === undefined ||
-      formFields.country === " " ||
-      formFields.country.length < 1
-    ) {
-      setValidCountry(false);
-    } else {
-      setValidCountry(true);
-    }
-    if (
-      formFields.contact === "" ||
-      formFields.contact === null ||
-      formFields.contact === undefined ||
-      formFields.contact === " " ||
-      formFields.contact.length < 10
-    ) {
-      setValidContact(false);
-    } else {
-      setValidContact(true);
-    }
-
-    if (
-      formFields.state === "" ||
-      formFields.state === null ||
-      formFields.state === undefined ||
-      formFields.state === " " ||
-      formFields.state.length < 1
-    ) {
-      setValidState(false);
-    } else {
-      setValidState(true);
-    }
-    if (
-      formFields.city === "" ||
-      formFields.city === null ||
-      formFields.city === undefined ||
-      formFields.city === " " ||
-      formFields.city.length < 1
-    ) {
-      setValidCity(false);
-    } else {
-      setValidCity(true);
-    }
-    if (
-      formFields.postaladdress === "" ||
-      formFields.postaladdress === null ||
-      formFields.postaladdress === undefined ||
-      formFields.postaladdress === " " ||
-      formFields.postaladdress.length < 1
-    ) {
-      setValidAddress(false);
-    } else {
-      setValidAddress(true);
     }
   };
 
--- client/src/components/context/form-field-context.js
+++ client/src/components/context/form-field-context.js
@@ -14,8 +14,123 @@ export const FormFieldProvider = ({ children }) => {
     postaladdress: "",
   });
 
+  const [validSubmittername, setValidSubmittername] = React.useState(null);
+  const [validRole, setValidRole] = React.useState(null);
+  const [validEmail, setValidEmail] = React.useState(null);
+  const [validCountry, setValidCountry] = React.useState(null);
+  const [validContact, setValidContact] = React.useState(null);
+  const [validState, setValidState] = React.useState(null);
+  const [validCity, setValidCity] = React.useState(null);
+  const [validAddress, setValidAddress] = React.useState(null);
+
+  const validateFields = () => {
+    if (
+      formFields.submittername === "" ||
+      formFields.submittername === null ||
+      formFields.submittername === undefined ||
+      formFields.submittername === " " ||
+      formFields.submittername.length < 1
+    ) {
+      setValidSubmittername(false);
+    } else {
+      setValidSubmittername(true);
+    }
+    if (
+      formFields.role === "" ||
+      formFields.role === null ||
+      formFields.role === undefined ||
+      formFields.role === " " ||
+      formFields.role.length < 1
+    ) {
+      setValidRole(false);
+    } else {
+      setValidRole(true);
+    }
+    if (
+      formFields.email === "" ||
+      formFields.email === null ||
+      formFields.email === undefined ||
+      formFields.email === " " ||
+      formFields.email.length < 1
+    ) {
+      setValidEmail(false);
+    } else {
+      setValidEmail(true);
+    }
+    if (
+      formFields.country === "" ||
+      formFields.country === null ||
+      formFields.country === undefined ||
+      formFields.country === " " ||
+      formFields.country.length < 1
+    ) {
+      setValidCountry(false);
+    } else {
+      setValidCountry(true);
+    }
+    if (
+      formFields.contact === "" ||
+      formFields.contact === null ||
+      formFields.contact === undefined ||
+      formFields.contact === " " ||
+      formFields.contact.length < 10
+    ) {
+      setValidContact(false);
+    } else {
+      setValidContact(true);
+    }
+
+    if (
+      formFields.state === "" ||
+      formFields.state === null ||
+      formFields.state === undefined ||
+      formFields.state === " " ||
+      formFields.state.length < 1
+    ) {
+      setValidState(false);
+    } else {
+      setValidState(true);
+    }
+    if (
+      formFields.city === "" ||
+      formFields.city === null ||
+      formFields.city === undefined ||
+      formFields.city === " " ||
+      formFields.city.length < 1
+    ) {
+      setValidCity(false);
+    } else {
+      setValidCity(true);
+    }
+    if (
+      formFields.postaladdress === "" ||
+      formFields.postaladdress === null ||
+      formFields.postaladdress === undefined ||
+      formFields.postaladdress === " " ||
+      formFields.postaladdress.length < 1
+    ) {
+      setValidAddress(false);
+    } else {
+      setValidAddress(true);
+    }
+  };
+
   return (
-    <FormFieldContext.Provider value={{ formFields, setFormFields }}>
+    <FormFieldContext.Provider
+      value={{
+        formFields,
+        setFormFields,
+        validSubmittername,
+        validRole,
+        validEmail,
+        validCountry,
+        validContact,
+        validState,
+        validCity,
+        validAddress,
+        validateFields,
+      }}
+    >
       {children}
     </FormFieldContext.Provider>
   );
